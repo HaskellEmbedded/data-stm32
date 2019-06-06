f1cb9f7cffbc00e2c2a8009e4995278c7fc14e01
Richard Marko <srk@48.io>
2019-06-06 19:47:12 +0200

    cmx: finish extractCMX(Cached)

--- src/Data/CMX/Extract.hs
+++ src/Data/CMX/Extract.hs
@@ -1,38 +1,66 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-module Data.CMX.Extract where
+module Data.CMX.Extract (
+    cmxFamilies
+  , cmxDevices
+  , extractCMX
+  , extractCMXCached
+  , filterSupported
+  ) where
 
+import Turtle
+import System.Exit
+import Prelude hiding (FilePath)
 import Control.Monad
 import qualified Data.Map as M
 import qualified Data.Set as S
+import qualified Data.ByteString.Char8 as B
+import Data.Serialize
 
+import Data.STM32.Types (Family(..), supportedFamilies)
 import Data.CMX.Types
 import Data.CMX.Parse
 
-cmxFamilies fs = M.keys
+cmxFamilies = M.keys
+cmxDevices  = concat . M.elems
 
-cmxDevices fs = concat
-  $ M.elems
-  $ M.map (concatMap (\x -> subFamMCUs x)) fs
+filterSupported :: M.Map Family [MCU] -> M.Map Family [MCU]
+filterSupported = M.filterWithKey (\k a -> k `elem` supportedFamilies)
 
-cmxDevicesRefNames :: Families -> [String]
-cmxDevicesRefNames = map smcuRefName . cmxDevices
-
-cmxDevicesXMLNames :: Families -> [String]
-cmxDevicesXMLNames = map smcuName . cmxDevices
-
-lol = do
-  let dbPath = "data"
+extractCMX :: String -> IO (M.Map Family [MCU])
+extractCMX dbPath = do
   fs <- parseFamilies (dbPath ++ "/db/mcu/families.xml")
-  p <- forM (take 1 $ cmxDevices fs) $ \dev -> do
-    mcu <- parseMCU $ dbPath ++ "/db/mcu/" ++ (smcuName dev) ++ ".xml"
-    return $ checkMCU $ fixMCU $ mcu {
-        mcuRam = smcuRam dev
-      , mcuFlash = smcuFlash dev
-      }
-    -- F0 [ShortMCU MCU]
-  -- print $ length p
-  return p
+  p <- forM (M.toList . M.map (concatMap subFamMCUs) $ fs) $ \(fam, devs) -> do
+    ds <- forM devs $ \dev -> do
+      mcu <- parseMCU $ dbPath ++ "/db/mcu/" ++ (smcuName dev) ++ ".xml"
+      return $ checkMCU $ fixMCU $ mcu {
+          mcuRam = smcuRam dev
+        , mcuFlash = smcuFlash dev
+        }
+
+    return (fam, ds)
+  return $ M.fromList p
+
+extractCMXCached :: String -> IO (M.Map Family [MCU])
+extractCMXCached dbPath = do
+  hasCache <- testfile $ decodeString $ cachePath
+  case hasCache of
+    True -> do
+      putStrLn $ "Loading cached CubeMX database from " ++ (show cachePath)
+      cached <- B.readFile cachePath
+      case decode cached of
+        Left err -> fail err
+        Right cmx -> return cmx
+
+    False -> do
+      putStrLn $ "Parsing CubeMX database from " ++ (show dbPath)
+      x <- extractCMX dbPath
+      putStrLn $ "Saving cache to " ++ (show cachePath)
+      B.writeFile cachePath (encode x)
+      return x
+  where
+    cachePath = "/tmp/data_stm32_cmx_cache"
+
 
 fixMCU x@MCU{..} = x { mcuIps = filterIps mcuIps }
   where
