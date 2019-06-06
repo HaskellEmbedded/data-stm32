b24bc77399e6f2d46e5a750b863701b84fdb1ec8
Richard Marko <srk@48.io>
2019-06-06 19:41:50 +0200

    default.nix: use nixpkgs.nix-gitignore.gitignoreSource when building locally

--- default.nix
+++ default.nix
@@ -1,2 +1,7 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" }:
-nixpkgs.haskell.packages.${compiler}.callPackage ./data-stm32.nix { }
+nixpkgs.haskell.lib.overrideCabal
+  (nixpkgs.haskell.packages.${compiler}.callPackage ./data-stm32.nix { })
+  ( oldDrv: {
+      src = nixpkgs.nix-gitignore.gitignoreSource [] ./.;
+    }
+)
