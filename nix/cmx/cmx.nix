6e9ce8d9e715963126d7292e7265e447e775d90e
Richard Marko <srk@48.io>
2020-01-07 15:21:09 +0100

    update cmx.nix

--- nix/cmx/cmx.nix
+++ nix/cmx/cmx.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/51/3b/ac/f0/ad/a3/44/c1/STM32CubeMX_v5-3-0/files/STM32CubeMX_v5-3-0.zip/jcr:content/translations/en.STM32CubeMX_v5-3-0.zip";
-  sha256 = "037c2f7ic2fv5ngzqcxk807y959z2a061jx3zysv4m3bm3mm9yha";
+  url = "https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/5f/d0/cf/79/10/fb/4e/7e/STM32CubeMX_v5-4-0/files/stm32cubemx_v5.4.0.zip/jcr:content/translations/en.stm32cubemx_v5.4.0.zip";
+  sha256 = "09v5qq6mkkjm369j5c1jk84crf0b4yri174z4n8b41qsdwpyzvgc";
 }
