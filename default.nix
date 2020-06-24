9f188c335d777c9539f5a886721c01fbdbb8bd2f
Richard Marko <srk@48.io>
2020-06-24 09:01:08 +0200

    default.nix: use ivory-tower-nix via itn.pkgs directly

--- default.nix
+++ default.nix
@@ -1,20 +1,18 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8101" }:
 
 let
-  ivory-tower-nix = nixpkgs.fetchFromGitHub {
+  itnSrc = nixpkgs.fetchFromGitHub {
     owner = "HaskellEmbedded";
     repo = "ivory-tower-nix";
     rev = "9ffb1710aff73a43e5059e5c718327bd3f644bac";
     sha256 = "0nrm0jax6iwjm78paw7jrq31jyi8kpd5qkaxl2dwvhpcv1n3ydpp";
   };
-
-  overlays = (import "${ivory-tower-nix}/overlay.nix" compiler);
-  pkgs = import <nixpkgs> { inherit overlays; };
+  itn = import itnSrc { inherit compiler nixpkgs; };
+  src = itn.pkgs.nix-gitignore.gitignoreSource [] ./.;
 in
-pkgs.haskell.lib.overrideCabal
-  (pkgs.myHaskellPackages.callPackage ./data-stm32.nix { })
+itn.pkgs.haskell.lib.overrideCabal
+  itn.ivorypkgs.data-stm32
   ( oldDrv: {
       doCheck = false;
-      src = pkgs.nix-gitignore.gitignoreSource [] ./.;
-    }
-)
+      inherit src;
+  })
