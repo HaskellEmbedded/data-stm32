aef2325c4cae44592ad8b32b86f967df018ad958
Richard Marko <srk@48.io>
2021-01-26 11:28:59 +0100

    bump ivory-tower-nix pin

--- default.nix
+++ default.nix
@@ -4,8 +4,8 @@ let
   itnSrc = nixpkgs.fetchFromGitHub {
     owner = "HaskellEmbedded";
     repo = "ivory-tower-nix";
-    rev = "00e5da396361978b791057555ed992d1118765d9";
-    sha256 = "094dbs0vlaqi32x7m4qxz7xgjqi8hdhifnag9vfy9p3zwb817rh6";
+    rev = "ccc4d98569d78c331df26bf9f387a7eb52c0c068";
+    sha256 = "1s2havlknh4dpc5q45i5c1cbp8j4kk9zj23d9br2v2qwa49d536g";
   };
   itn = import itnSrc { inherit compiler nixpkgs; };
   src = itn.pkgs.nix-gitignore.gitignoreSource [] ./.;
