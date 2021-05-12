367ccfa0a1721f6c37807ddf9291c840e6e4ef3b
Richard Marko <srk@48.io>
2021-05-12 11:07:01 +0200

    STM32/Drivers: add comments, annotations, hint warnings

--- src/Data/STM32/Drivers.hs
+++ src/Data/STM32/Drivers.hs
@@ -1,31 +1,46 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RecordWildCards #-}
 module Data.STM32.Drivers where
 
-import Data.Maybe
-import qualified Data.Set as S
-import qualified Data.List as L
+import qualified Data.Maybe
+import qualified Data.Set
+import qualified Data.List
 import Data.STM32.Types
 import Data.CMX
 
-data RegTypes = NoRegTypes | CommonRegTypes | VersionedRegTypes
+-- | Are register types (Peripheral/RegTypes.hs)
+-- for this peripheral common for each driver or versioned?
+data RegTypes =
+    NoRegTypes        -- ^ Has no register types
+  | CommonRegTypes    -- ^ Common register types for all versions
+  | VersionedRegTypes -- ^ Versioned register types
   deriving (Show, Eq)
 
-data Driver = NoDriver | CommonDriver | VersionedDriver
+-- | Is driver for this peripheral common or versioned
+data Driver =
+    NoDriver         -- ^ No driver (e.g. GPIO)
+  | CommonDriver     -- ^ Common driver for all versions
+  | VersionedDriver  -- ^ Versioned drivers
   deriving (Show, Eq)
 
-data Match = ByFam Family | ByDev String | ByIP String | Not Match
+-- | Match devices
+data Match =
+    ByFam Family -- ^ By @Family@
+  | ByDev String -- ^ By device string
+  | ByIP String  -- ^ By IP block version
+  | Not Match    -- ^ Negated Match
   deriving (Show, Eq)
 
+-- | Description of a specific driver for @Periph@
 data DriverInfo = DriverInfo {
-    diPeriph :: Periph
-  , diVersion :: Maybe Int
-  , diRepresentative :: Match
-  , diCompatible :: [Match]
-  , diRegTypes :: RegTypes
-  , diDriver :: Driver
+    diPeriph         :: Periph     -- ^ Peripheral
+  , diVersion        :: Maybe Int  -- ^ @Nothing@ if there's only one driver version, @Just version@ for versioned
+  , diRepresentative :: Match      -- ^ Representative device using this driver, used for generating registers
+  , diCompatible     :: [Match]    -- ^ Compatible devices, if empty only @diRepresentative@ matches
+  , diRegTypes       :: RegTypes   -- ^ Versioned, common or no register types (RegTypes.hs)
+  , diDriver         :: Driver     -- ^ Versioned, common or no driver
   } deriving (Show)
 
+driverMapping :: [DriverInfo]
 driverMapping = [
     DriverInfo CAN Nothing (ByIP "bxcan1_v1_1")
       [ByIP "bxcan1_v1_1_F7"] CommonRegTypes CommonDriver
@@ -88,36 +103,57 @@ driverMapping = [
       [ByIP "exti_g0"] CommonRegTypes VersionedDriver
   ]
 
+-- | List of drivers for specific @Periph@
+periphDrivers :: Periph -> [DriverInfo]
 periphDrivers p = filter (\DriverInfo{..} -> diPeriph == p) driverMapping
 
-versioned' p = 1 < (length $ filter (\DriverInfo{..} -> diPeriph == p) driverMapping)
+-- | Is this @Periph@ versioned? Meaning has it more than one version across all devices?
+versioned' :: Periph -> Bool
+versioned' p = 1 < length (filter (\DriverInfo{..} -> diPeriph == p) driverMapping)
 
+-- | List of driver versions for @Periph@
+driverVersions :: Periph -> [Maybe Int]
 driverVersions = map diVersion . periphDrivers
 
+-- | Supported peripherals (peripherals with known driver).
+-- @UART@ is listed here since it is treated similar to @USART@ peripheral
 supported :: [Periph]
-supported = [UART] ++ (L.nub . map diPeriph $ driverMapping)
+supported = UART:(Data.List.nub . map diPeriph $ driverMapping)
 
-mcuPeriphDriver mcu periph = case periph of
-  _ -> case filter (mcuCompatible mcu) (periphDrivers $ asUSART periph) of
-          [x] -> Just x
-          _   -> Nothing
+-- | Try to find @DriverInfo@ for concrete @MCU@ and @Periph@
+mcuPeriphDriver :: MCU -> Periph -> Maybe DriverInfo
+mcuPeriphDriver mcu periph = case filter (mcuCompatible mcu) (periphDrivers $ asUSART periph) of
+  [x] -> Just x
+  _   -> Nothing
   where asUSART UART = USART
         asUSART x = x
 
+-- | Is this @MCU@ compatible with @DriverInfo@?
+mcuCompatible :: MCU -> DriverInfo -> Bool
 mcuCompatible mcu DriverInfo{..} = any (isCompat mcu) (diRepresentative:diCompatible)
 
 -- XXX: Turn dev into like (st # family F7 # dev "65")
 -- and turn that into ByIP and call isCompat again.
 -- Then we can use (ByName "F405") in `diCompatible` as well.
+isCompat :: MCU -> Match -> Bool
 isCompat MCU{..} (ByDev name) = matches ("STM32" ++ name) mcuName
 isCompat MCU{..} (ByFam f) = mcuFamily == f
 isCompat x (Not m) = not $ isCompat x m
-isCompat MCU{..} (ByIP name) = S.member name (S.map ipVersion mcuIps)
+isCompat MCU{..} (ByIP name) = Data.Set.member name (Data.Set.map ipVersion mcuIps)
 
-hasPeriph mcu periph =  not . S.null . S.filter (\ip -> ipName ip == show periph) $ mcuIps mcu
+-- | True if this @MCU@ has @Periph@
+hasPeriph :: MCU -> Periph -> Bool
+hasPeriph mcu periph =
+    not
+  . Data.Set.null
+  . Data.Set.filter (\ip -> ipName ip == show periph)
+  $ mcuIps mcu
 
-hasDriver mcu periph = isJust $ mcuPeriphDriver mcu periph
+-- | True if there's a known driver for @MCU@s @Periph@
+hasDriver :: MCU -> Periph -> Bool
+hasDriver mcu periph = Data.Maybe.isJust $ mcuPeriphDriver mcu periph
 
+-- | Find some @MCU@ for specified @DriverInfo@
 someMCUWithDriver :: DriverInfo -> [MCU] -> MCU
 someMCUWithDriver di ms =
   case filter (\mcu -> isCompat mcu (diRepresentative di)) ms of
