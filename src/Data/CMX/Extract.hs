a19202ccdde8ead136d130007b258e61ef0a773d
Richard Marko <srk@48.io>
2021-05-13 09:59:40 +0200

    CMX.Extract: hints

--- src/Data/CMX/Extract.hs
+++ src/Data/CMX/Extract.hs
@@ -19,12 +19,13 @@ module Data.CMX.Extract (
   , fx
   ) where
 
-import Turtle
-import System.Exit
+import Turtle hiding (x)
 import Prelude hiding (FilePath)
 import Control.Monad
 import qualified Data.List as L
 import qualified Data.Map as M
+import qualified Data.Maybe
+import qualified Data.Ord
 import qualified Data.Set as S
 import qualified Data.ByteString.Char8 as B
 import Data.Serialize
@@ -37,11 +38,13 @@ import Data.STM32.Periph
 import Data.CMX.Types
 import Data.CMX.Parse
 
+cmxFamilies :: M.Map k a -> [k]
 cmxFamilies = M.keys
+cmxDevices :: M.Map k [a] -> [a]
 cmxDevices  = concat . M.elems
 
 filterSupported :: M.Map Family [MCU] -> M.Map Family [MCU]
-filterSupported = M.filterWithKey (\k a -> k `elem` supportedFamilies)
+filterSupported = M.filterWithKey (\k _ -> k `elem` supportedFamilies)
 
 extractCMX :: FilePath -> IO (M.Map Family [MCU], M.Map String AlternateFunctions)
 extractCMX dbPath = do
@@ -49,7 +52,7 @@ extractCMX dbPath = do
   xs <- forM (M.toList . M.map (concatMap subFamMCUs) $ fs) $ \(fam, devs) -> do
     ds <- forM devs $ \dev -> do
       putStrLn $ "Parsing " ++ smcuName dev
-      mcu <- parseMCU (encodeString dbPath ++ "/db/mcu/" ++ (smcuName dev) ++ ".xml") (smcuRefName dev)
+      mcu <- parseMCU (encodeString dbPath ++ "/db/mcu/" ++ smcuName dev ++ ".xml") (smcuRefName dev)
       let m = checkMCU $ fixMCU $ mcu {
                 mcuRam = smcuRam dev
               , mcuFlash = smcuFlash dev
@@ -66,7 +69,7 @@ extractCMX dbPath = do
     return (fam, ds)
 
   afModes <- extractAlternateFunctions dbPath (concatMap snd xs)
-  return $ (M.fromList xs, afModes)
+  return (M.fromList xs, afModes)
 
 extractAlternateFunctions :: FilePath -> [MCU] -> IO (M.Map String AlternateFunctions)
 extractAlternateFunctions dbPath mcus = do
@@ -80,21 +83,23 @@ extractAlternateFunctions dbPath mcus = do
 extractCMXCached :: FilePath -> IO (M.Map Family [MCU], M.Map String AlternateFunctions)
 
 extractCMXCached dbPath = do
-  hasCache <- testfile $ decodeString $ cachePath
-  case hasCache of
-    True -> do
+  hasCache <- testfile $ decodeString cachePath
+  if hasCache
+    then
+    (do
       putStrLn $ "Loading cached CubeMX database from " ++ cachePath
       cached <- B.readFile cachePath
       case decode cached of
-        Left err -> fail err
-        Right cmx -> return cmx
+        Left em -> fail em
+        Right cmx -> return cmx)
 
-    False -> do
-      putStrLn $ "Parsing CubeMX database from " ++ (encodeString dbPath)
+    else
+    (do
+      putStrLn $ "Parsing CubeMX database from " ++ encodeString dbPath
       x <- extractCMX dbPath
       putStrLn $ "Saving cache to " ++ cachePath
       B.writeFile cachePath (encode x)
-      return x
+      return x)
   where
     cachePath = "/tmp/data_stm32_cmx_cache"
 
@@ -105,7 +110,8 @@ fixMCU x@MCU{..} = x { mcuIps = addEXTI . coerceIPVersions . filterIps $ mcuIps 
     dropCMXSuffix x | "_Cube" `L.isSuffixOf` x = take (length x - length ("_Cube" :: String)) x
     dropCMXSuffix x = x
     filterIps = S.filter (\IP{..} ->
-      not $ elem (map toUpper ipName)
+      map toUpper ipName
+      `notElem`
           [ "CORTEX-M7"
           , "GRAPHICS"
           , "GFXSIMULATOR"
@@ -136,31 +142,42 @@ fixMCU x@MCU{..} = x { mcuIps = addEXTI . coerceIPVersions . filterIps $ mcuIps 
 
 
 -- fill in missing ram2 and ram3 sizes, compute real sram1 size
-adjustRAMs nmcu@(name, mcu) = checkRAM $ calcRam1 $ addCcmRam $ (name, mcu {
+adjustRAMs :: (STM32DevName, MCU) -> (STM32DevName, MCU)
+adjustRAMs nmcu@(name, mcu) = checkRAM $ calcRam1 $ addCcmRam (name, mcu {
     mcuRam2 = ram2Size nmcu
   , mcuRam3 = ram3Size nmcu
   })
 
 -- compute ram1 size - we substract ram2 & ram3 & ccram from
 -- total size
+calcRam1 :: (STM32DevName, MCU) -> (STM32DevName, MCU)
 calcRam1 (name, m@MCU{..}) = (name, m { mcuRam1 = ram1 })
   where
     total = mcuRam
     ram1 = total - (ram2 + ram3 + ccram)
-    ram2 = maybe 0 id mcuRam2
-    ram3 = maybe 0 id mcuRam3
-    ccram = maybe 0 id mcuCcmRam
+    ram2 = Data.Maybe.fromMaybe 0 mcuRam2
+    ram3 = Data.Maybe.fromMaybe 0 mcuRam3
+    ccram = Data.Maybe.fromMaybe 0 mcuCcmRam
 
 -- for some MCUs we don't have CCM RAM size information in cubemx files
 -- so we add it manually based on `Data.STM32.Memory.ccmSize`
+addCcmRam :: (STM32DevName, MCU) -> (STM32DevName, MCU)
 addCcmRam nmcu@(name, mcu) = (name, maybeSetCcm mcu (ccmSize nmcu))
   where
-    maybeSetCcm mcu (Just _size) = mcu { mcuCcmRam = ccmSize nmcu }
-    maybeSetCcm mcu Nothing = mcu
+    maybeSetCcm mcu' (Just _size) = mcu' { mcuCcmRam = ccmSize nmcu }
+    maybeSetCcm mcu' Nothing = mcu'
 
-checkRAM nmcu@(name, MCU{..}) | mcuRam == mcuRam1 + (maybe 0 id mcuRam2) + (maybe 0 id mcuRam3) + (maybe 0 id mcuCcmRam) = nmcu
-checkRAM (name, _) | otherwise = error $ "ram1 + ram2 + ram3 + ccram is not equal mcuRam" ++ showName name
+checkRAM :: (STM32DevName, MCU) -> (STM32DevName, MCU)
+checkRAM nmcu@(_name, MCU{..})
+  | mcuRam ==
+      mcuRam1
+    + Data.Maybe.fromMaybe 0 mcuRam2
+    + Data.Maybe.fromMaybe 0 mcuRam3
+    + Data.Maybe.fromMaybe 0 mcuCcmRam = nmcu
+checkRAM (name, _)
+  = error $ "ram1 + ram2 + ram3 + ccram is not equal mcuRam" ++ showName name
 
+checkMCU :: MCU -> MCU
 checkMCU MCU{..} | mcuFlash == 0 && mcuFamily /= MP1 = error $ "MCU Flash is 0 @" ++ mcuRefName
 checkMCU MCU{..} | mcuRam   == 0 = error $ "MCU Ram is 0 @" ++ mcuRefName
 checkMCU x = x
@@ -191,10 +208,9 @@ mcusWithIp p = filter (hasIp p) . cmxDevices
 
 -- returns ip with the count of devices which use it
 ipPerMCUCounts :: M.Map Family [MCU] -> [((String, String), Int)]
-ipPerMCUCounts devs = reverse
-                    $ L.sortOn snd
+ipPerMCUCounts devs = L.sortOn (Data.Ord.Down . snd)
                     $ map (\p -> (p, length $ mcusWithIp p devs))
-                    $ S.toList (uniqueIps $ devs)
+                    $ S.toList (uniqueIps devs)
 
 -- returns ip with the list of families which use it
 ipFamilies :: M.Map Family [MCU] -> [((String, String), [Family])]
@@ -204,23 +220,24 @@ ipFamilies devs = map (\ip -> (ip, L.nub $ map mcuFamily $ mcusWithIp ip devs))
 -- filter devices starting with this prefix
 -- fx (cmxs db) "F405"
 fx :: M.Map Family [MCU] -> String -> [MCU]
-fx db x = filter (\m -> ("STM32" ++ x) `L.isPrefixOf` (mcuRefName m)) . cmxDevices $ db
+fx db x = filter (\m -> ("STM32" ++ x) `L.isPrefixOf` mcuRefName m) . cmxDevices $ db
 
 getIPVersion :: Periph -> MCU -> String
 getIPVersion p m = case S.toList $ S.map ipVersion $ S.filter ((== show p) . ipName) $ mcuIps m of
-  [] -> error $ "No IP found with name " ++ (show p)
+  [] -> error $ "No IP found with name " ++ show p
   [x] -> x
-  xs -> error $ "Multiple IPs found with name " ++ (show p) ++ " found " ++ show xs
+  xs -> error $ "Multiple IPs found with name " ++ show p ++ " found " ++ show xs
 
 -- STM32F091_gpio_v1_0 -> F091
 ipShortName :: Periph -> MCU -> String
 ipShortName periph mcu = takeWhile (/='_') . drop 5 $ getIPVersion periph mcu
 
+-- | Get a path to peripherals IP description xml
 ipXMLPath :: FilePath -> Periph -> MCU -> String
 ipXMLPath dbPath periph mcu = concat
   [ encodeString dbPath
   , "/db/mcu/IP/"
-  , (show periph)
+  , show periph
   , "-"
   , getIPVersion periph mcu
   , "_Modes.xml" ]
