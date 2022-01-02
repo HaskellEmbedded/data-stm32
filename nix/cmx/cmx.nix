aafc8af5af5f0692e8c0144088e5da03062d750b
Richard Marko <srk@48.io>
2022-01-02 13:27:04 +0100

    update cmx to 6.4.0

--- nix/cmx/cmx.nix
+++ nix/cmx/cmx.nix
@@ -1,4 +1,4 @@
 {
-  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v621-lin.zip";
-  sha256 = "0971qvmigxxfky6jvp0gj66dswzczcjwrjcnxqfgxzhwx90a0zw9";
+  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v640-lin.zip";
+  sha256 = "1x7cy21awmzlgag2w186fii909hs5f6yasg1kg7apvflij5nh5f5";
 }
