5128800d2b559c9fa6df2b98a95a7bf920dc2fb3
Richard Marko <srk@48.io>
2022-01-02 13:25:33 +0100

    update cmx to 6.2.1

--- nix/cmx/cmx.nix
+++ nix/cmx/cmx.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/0b/05/f0/25/c7/2b/42/9d/stm32cubemx_v6-1-1/files/stm32cubemx_v6-1-1.zip/jcr:content/translations/en.stm32cubemx_v6-1-1.zip";
-  sha256 = "1xk90br6d5ilz6mb3qch11b13z6pvfw7cs0f8ma9km8agccmmzc0";
+  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v621-lin.zip";
+  sha256 = "0971qvmigxxfky6jvp0gj66dswzczcjwrjcnxqfgxzhwx90a0zw9";
 }
