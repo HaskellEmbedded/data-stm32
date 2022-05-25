4b37fe6641d6a031855d9fcdfa6e1b590a2fe3ae
Richard Marko <srk@48.io>
2022-05-25 19:10:29 +0200

    update cmx

--- nix/cmx/cmx.nix
+++ nix/cmx/cmx.nix
@@ -1,4 +1,4 @@
 {
-  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v640-lin.zip";
-  sha256 = "1x7cy21awmzlgag2w186fii909hs5f6yasg1kg7apvflij5nh5f5";
+  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v650-lin.zip";
+  sha256 = "0dvhkqag52xj0drv1yq0nrwlbrkxfirxxxwgl2pmxhc18mixcya3";
 }
