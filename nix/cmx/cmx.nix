9cbd75797249f9a87708bd05014ecbcbd2dc5137
sorki <srk@48.io>
2023-12-25 08:39:13 +0100

    update cmx.nix (v650 -> v6100)

--- nix/cmx/cmx.nix
+++ nix/cmx/cmx.nix
@@ -1,4 +1,4 @@
 {
-  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v650-lin.zip";
-  sha256 = "0dvhkqag52xj0drv1yq0nrwlbrkxfirxxxwgl2pmxhc18mixcya3";
+  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v6100-lin.zip";
+  sha256 = "0bz0m17w9pwm1hi4fwx1c21jmrw4fpcvyyrbz1k7idzgzrhx96g0";
 }
