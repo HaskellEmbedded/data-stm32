6bb784757ed7e6ea531a3cc79f00b1167f76cbd0
sorki <srk@48.io>
2024-02-14 09:53:12 +0100

    add DMA UART drivers for V2 and V3 UARTs

---
+++ templates/STM32/Peripheral/UART/DMA.hs
@@ -0,0 +1,14 @@
+
+module Ivory.BSP.STM32.Peripheral.UART.DMA where
+
+import Ivory.BSP.STM32.Peripheral.UART
+import Ivory.BSP.STM32.Peripheral.DMA
+
+data DMAUART = DMAUART
+  { dmaUARTPeriph    :: UART
+  , dmaUARTDMAPeriph :: DMA
+  , dmaUARTTxStream  :: DMAStream -- int 0..7
+  , dmaUARTTxChannel :: DMAChannel -- int 0..7
+  , dmaUARTRxStream  :: DMAStream -- int 0..7
+  , dmaUARTRxChannel :: DMAChannel -- int 0..7
+  }
