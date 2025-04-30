a6bb926529c1dc46464d534e2a8c1fefb5c5269c
sorki <srk@48.io>
2025-04-30 08:15:01 +0200

    Add ETHConfig

--- templates/STM32/Peripheral/ETH.hs
+++ templates/STM32/Peripheral/ETH.hs
@@ -4,6 +4,7 @@ module Ivory.BSP.STM32.Peripheral.ETH
   , module Ivory.BSP.STM32.Peripheral.ETH.Peripheral
   , module Ivory.BSP.STM32.Peripheral.ETH.Pins
   , module Ivory.BSP.STM32.Peripheral.ETH.Phy
+  , ETHConfig(..)
   ) where
 
 import Ivory.BSP.STM32.Peripheral.ETH.Descriptor
@@ -11,3 +12,8 @@ import Ivory.BSP.STM32.Peripheral.ETH.Descriptor.Types
 import Ivory.BSP.STM32.Peripheral.ETH.Peripheral
 import Ivory.BSP.STM32.Peripheral.ETH.Pins
 import Ivory.BSP.STM32.Peripheral.ETH.Phy
+
+data ETHConfig = ETHConfig
+  { ethConfigPeriph :: ETH
+  , ethConfigPins   :: ETHPins
+  }
