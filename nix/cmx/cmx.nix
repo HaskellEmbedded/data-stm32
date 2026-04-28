84c7eea8428f017dcb911eaef08bbe9d43524851
sorki <srk@48.io>
2026-04-28 09:36:10 +0200

    bump cmx

--- nix/cmx/cmx.nix
+++ nix/cmx/cmx.nix
@@ -1,4 +1,4 @@
 {
-  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v6100-lin.zip";
-  sha256 = "0bz0m17w9pwm1hi4fwx1c21jmrw4fpcvyyrbz1k7idzgzrhx96g0";
+  url = "http://sw-center.st.com/packs/resource/library/stm32cube_mx_v6170-lin.zip";
+  sha256 = "0207c8kng0swnbgpiw9q2llg51kq9v5ch2i3sqrsb97xwx6ziqiy";
 }
